7a423be6dc70046740c210690ee0a6c389c565bd
lzaragosa555459-eng <l.zaragosa.555459@umindanao.edu.ph>
2026-06-16 13:08:18 +0800

    update: added crud in users module

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,10 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database.db');
+const db = require("../config/database");
 
 exports.index = (req, res) => {
 
-    db.all(`
+    const sql = `
         SELECT
-            users.id,
-            users.username,
-            users.role,
+            users.*,
             employees.employee_id,
             employees.name,
             employees.position,
@@ -18,11 +15,13 @@ exports.index = (req, res) => {
             ON users.employee_id = employees.id
         LEFT JOIN departments
             ON employees.department_id = departments.id
-    `, [], (err, users) => {
+    `;
+
+    db.all(sql, [], (err, users) => {
 
         if (err) {
-            console.error(err);
-            return res.status(500).send("DB Error");
+            console.log(err);
+            return res.send("Database Error");
         }
 
         res.render("users", {
@@ -30,19 +29,145 @@ exports.index = (req, res) => {
             layout: "layouts/main",
             users
         });
-
     });
+};
 
+exports.create = (req, res) => {
+
+    db.all(
+        `SELECT id, employee_id, name FROM employees`,
+        [],
+        (err, employees) => {
+
+            if (err) {
+                console.log(err);
+                return res.send("Database Error");
+            }
+
+            res.render("users/add", {
+                title: "Add User",
+                layout: "layouts/main",
+                employees
+            });
+        }
+    );
 };
 
 exports.store = (req, res) => {
-    res.send("User Created");
+
+    const {
+        username,
+        password,
+        role,
+        employee_id
+    } = req.body;
+
+    db.run(
+        `
+        INSERT INTO users
+        (username, password, role, employee_id)
+        VALUES (?, ?, ?, ?)
+        `,
+        [
+            username,
+            password,
+            role,
+            employee_id || null
+        ],
+        (err) => {
+
+            if (err) {
+                console.log(err);
+                return res.send(err.message);
+            }
+
+            res.redirect("/users");
+        }
+    );
+};
+
+exports.edit = (req, res) => {
+
+    db.get(
+        `SELECT * FROM users WHERE id = ?`,
+        [req.params.id],
+        (err, user) => {
+
+            if (err) {
+                console.log(err);
+                return res.send("Database Error");
+            }
+
+            db.all(
+                `SELECT id, employee_id, name FROM employees`,
+                [],
+                (err, employees) => {
+
+                    if (err) {
+                        console.log(err);
+                        return res.send("Database Error");
+                    }
+
+                    res.render("users/edit", {
+                        title: "Edit User",
+                        layout: "layouts/main",
+                        user,
+                        employees
+                    });
+                }
+            );
+        }
+    );
 };
 
 exports.update = (req, res) => {
-    res.send("User Updated");
+
+    const {
+        username,
+        role,
+        employee_id
+    } = req.body;
+
+    db.run(
+        `
+        UPDATE users
+        SET
+            username = ?,
+            role = ?,
+            employee_id = ?
+        WHERE id = ?
+        `,
+        [
+            username,
+            role,
+            employee_id || null,
+            req.params.id
+        ],
+        (err) => {
+
+            if (err) {
+                console.log(err);
+                return res.send(err.message);
+            }
+
+            res.redirect("/users");
+        }
+    );
 };
 
 exports.destroy = (req, res) => {
-    res.send("User Deleted");
+
+    db.run(
+        `DELETE FROM users WHERE id = ?`,
+        [req.params.id],
+        (err) => {
+
+            if (err) {
+                console.log(err);
+                return res.send(err.message);
+            }
+
+            res.redirect("/users");
+        }
+    );
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,13 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/", userController.index);
-router.post("/", userController.store);
-router.put("/:id", userController.update);
-router.delete("/:id", userController.destroy);
+
+router.get("/add", userController.create);
+router.post("/store", userController.store);
+
+router.get("/edit/:id", userController.edit);
+router.post("/update/:id", userController.update);
+
+router.get("/delete/:id", userController.destroy);
 
 module.exports = router;
